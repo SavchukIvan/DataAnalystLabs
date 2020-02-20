62d809aea0b1338ac0f70dc2896460543ebafc22
SavchukIvan <31822421+SavchukIvan@users.noreply.github.com>
2020-02-20 17:26:15 +0200

    bla2

--- lab1/disp.py
+++ lab1/disp.py
@@ -2,6 +2,44 @@ import numpy as np
 import pandas as pd
 
 
+def twoway_anova(df):
+    dff = prep(df)
+    a = []
+    b = []
+    for i in range(len(dff.columns)):
+        a.append(sum(dff[i].values))
+    for i in range(len(dff[0].values)):
+        b.append(sum(dff.loc[i, :]))
+    print(sum(a), sum(b))
+
+
+def prep(df):
+    sl = int(len(df[1].values)/4)
+    dff = []
+    for i in range (len(df.columns)):
+        dff.append(df[i].values)
+    dff1 = []
+    dff2 = []
+    dff3 = []
+    dff4 = []
+    dffn = [dff1, dff2, dff3, dff4]
+    for j in range (len(df.columns)):
+        dff1.append(dff[j][:sl])
+        dff2.append(dff[j][sl:(2*sl)])
+        dff3.append(dff[j][(2*sl):(3 * sl)])
+        dff4.append(dff[j][(3*sl):(4 * sl)])
+    a = []
+    for i in range(len(dffn)):
+        b = [np.mean(dffn[i][j]) for j in range(len(dffn[i]))]
+        a.append(b)
+    names = [0, 1, 2, 3]
+    zipped = list(zip(names, a))
+    data = dict(zipped)
+    stats_t = pd.DataFrame(data)
+    stats = stats_t.transpose()
+    return stats
+    
+
 def anova(df):
     s_per_col = []
     q1 = 0
@@ -31,7 +69,7 @@ def variance(df):
     zipped = list(zip(name, [varc]))
     data = dict(zipped)
     stats = pd.DataFrame(data)
-    #print(stats)
+    print(stats)
 
     g = max(varc) / sum(varc)
     print(g)
@@ -42,5 +80,7 @@ if __name__ == "__main__":
     a = pd.read_csv('kpi17.txt', sep='\s+', header=None)
     df = a.loc[:, a.columns != 0]
     df.columns = [i for i in range(6)]
-    print(df.head(4))
-    anova(df)
+    #variance(df)
+    #print(df.head(4))
+    #anova(df)
+    twoway_anova(df)
